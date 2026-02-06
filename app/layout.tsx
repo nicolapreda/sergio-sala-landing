1fa7436c5f671f22750afb75bdec8c109d380807
Nicola Preda <nicola.preda05@gmail.com>
2026-02-06 02:16:18 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,22 @@ export default function RootLayout({
             `,
           }}
         />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-RHPNX0PHT2"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RHPNX0PHT2');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         {children}
